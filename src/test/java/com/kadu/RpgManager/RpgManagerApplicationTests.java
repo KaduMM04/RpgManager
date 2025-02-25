2d8a0e4cf497e87e1f77a008ad907ce3e374dff7
Kadu Miranda <kadumendesmiranda@gmail.com>
2025-02-25 15:54:21 -0300

    Project Created

---
+++ src/test/java/com/kadu/RpgManager/RpgManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kadu.RpgManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpgManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
