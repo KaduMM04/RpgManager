a0bc4a28c3118fbd7b7d14f5eb3d951d7138bc9a
Kadu Miranda <kadumendesmiranda@gmail.com>
2025-03-07 00:06:49 -0300

    Change imports

--- src/main/java/com/kadu/RpgManager/controllers/RpgStoryController.java
+++ src/main/java/com/kadu/RpgManager/controllers/RpgStoryController.java
@@ -9,13 +9,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kadu.RpgManager.dtos.RpgStoryDTO;
-import com.kadu.RpgManager.dtos.UserDTO;
 import com.kadu.RpgManager.services.RpgStoryService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 
